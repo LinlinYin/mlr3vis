b4a538f62b89c03ecd6ceffac082ee1c943de931
Linlin Yin <linlinyin@outlook.com>
2019-06-24 01:10:13 -0500

    a lot of changes based on issues

--- R/plotLearnerPrediction.R
+++ R/plotLearnerPrediction.R
@@ -148,3 +148,81 @@ plotLearnerPrediction = function(learner, task, features = NULL, cv = 10L, err.m
 
 
 }
+
+#' @export
+
+plotLearnerPredictionExperiment = function(e, features = NULL, err.mark = "train",
+                                 pointsize = 2, err.size = pointsize, err.col = "white", prob.alpha = TRUE,
+                                 gridsize = 100L) {
+
+  target = e$task$target_names
+  predictionResult=as.data.table(e$prediction)
+  predictionResult$.err = predictionResult$response != predictionResult$truth
+  colnames(predictionResult)[2] = target
+
+  fns = e$task$feature_names
+  if (is.null(features)) {
+    features = if(length(fns) == 1L) fns else fns[1:2]
+  }
+  dataForPlot = e$task$data()[, features, with = FALSE]
+
+  # X1 and X2, and grid
+  x1n = features[1L]
+  x1 = dataForPlot[, get(x1n)]
+  taskdim = length(features)
+  if (taskdim == 2L) {
+    x2n = features[2L]
+    x2 = dataForPlot[, get(x2n)]
+  }
+  if (taskdim == 1L) {
+    grid = data.frame(x = seq(min(x1), max(x1), length.out = gridsize))
+  } else if (taskdim == 2L) {
+    # setup data frames for ggplot. grid = background, data = points
+    grid = expand.grid(
+      seq(min(x1), max(x1), length.out = gridsize),
+      seq(min(x2), max(x2), length.out = gridsize)
+    )
+  }
+  colnames(grid) = features
+  #
+  # # grid for predictions
+  # grid = cbind(grid, levels(e$task$truth())[1])
+  # colnames(grid)[ncol(grid)] = target
+  # pred.grid = e$predict(newdata = grid)
+  # grid[, target] = pred.grid$prediction$response
+
+
+
+
+
+
+  dataForPlot=cbind(dataForPlot,predictionResult)
+
+  p = ggplot(grid, aes_string(x = x1n, y = x2n))
+
+  ## plot correct points
+  p = p + geom_point(data = subset(dataForPlot, !dataForPlot$.err),
+                     mapping = aes_string(x = x1n, y = x2n, shape = target), size = pointsize)
+  #  p = ggplot(data = subset(dataForPlot, !dataForPlot$.err),
+  #             mapping = aes_string(x = x1n, y = x2n, shape = "truth"), size = pointsize)+geom_point()
+
+  # plot error mark on error points
+  if (err.mark != "none" && any(dataForPlot$.err)) {
+    p = p + geom_point(data = subset(dataForPlot, dataForPlot$.err),
+                       mapping = aes_string(x = x1n, y = x2n, shape = target),
+                       size = err.size + 1.5, show.legend = FALSE)
+    p = p + geom_point(data = subset(dataForPlot, dataForPlot$.err),
+                       mapping = aes_string(x = x1n, y = x2n, shape = target),
+                       size = err.size + 1, col = err.col, show.legend = FALSE)
+  }
+
+  # plot error points
+  p = p + geom_point(data = subset(dataForPlot, dataForPlot$.err),
+                     mapping = aes_string(x = x1n, y = x2n, shape = target),  size = err.size, show.legend = FALSE)
+  p = p + guides(alpha = FALSE)
+  return(p)
+
+}
+
+
+
--- README.md
+++ README.md
@@ -8,11 +8,7 @@ remotes::install_github("LinlinYin/mlr3vis")
 
 ## Document under Development
 
-https://github.com/LinlinYin/mlr3vis/blob/master/inst/mlr3vis_dev.Rmd
-
-## Document preview
-
-http://htmlpreview.github.io/?https://github.com/LinlinYin/mlr3vis/blob/master/inst/mlr3vis_dev.html
+https://github.com/LinlinYin/mlr3vis/blob/master/vignettes/mlr3vis_dev.Rmd
 
 
 

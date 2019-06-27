75fcfce14cf0d474cf74bf7e2bc21c6baa59e2c5
Linlin Yin <linlinyin@outlook.com>
2019-06-26 23:38:52 -0500

    tuning dev and documents

--- R/generateThreshVsPerfData.R
+++ R/generateThreshVsPerfData.R
@@ -25,3 +25,31 @@ generateThreshVsPerfData = function(e, gridsize = 100L) {
 
   return(performanceByThreshold)
 }
+
+#' @title Generate threshold vs. performance(s) for 2-class classification in Benchmark object.
+#'
+#' @description
+#' Generates data on threshold vs. performance(s) for 2-class classification that can be used for plotting.
+#'
+#'
+#' @param bmr Benchmark object.
+#' @param gridsize Grid resolution for x-axis (threshold). Default is 100.
+#' @export
+#' @examples
+#' 1
+generateThreshVsPerfDataInBenchmark=function(bmr, gridsize = 100L) {
+  performanceData=NULL
+
+  bmrAgg = bmr$aggregated(objects = FALSE)
+  for (i in 1:nrow(bmrAgg)) {
+      rr=bmrAgg[i,]$resample_result[[1]]
+      task_id=bmrAgg$task_id[i]
+      learner_id=bmrAgg$learner_id[i]
+      performanceDataOne=generateThreshVsPerfData(rr$experiments()[[1]],gridsize=gridsize)
+      performanceDataOne=cbind(bmrAgg[i,c("hash","task_id","learner_id","resampling_id")],performanceDataOne)
+      performanceData=rbind(performanceData,performanceDataOne)
+  }
+  return(performanceData)
+}
+
+
